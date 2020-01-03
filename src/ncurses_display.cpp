c69b1efd8f339c800196fea7a7124bd82edb85b5
Immanuel Baur <immanuel.baur@daimler.com>
2020-01-02 20:30:13 -0800

    fixing error to build on mac os

--- src/ncurses_display.cpp
+++ src/ncurses_display.cpp
@@ -1,11 +1,13 @@
+#include "ncurses_display.h"
+
 #include <curses.h>
+
 #include <chrono>
 #include <string>
 #include <thread>
 #include <vector>
 
 #include "format.h"
-#include "ncurses_display.h"
 #include "system.h"
 
 using std::string;
@@ -77,8 +79,10 @@ void NCursesDisplay::DisplayProcesses(std::vector<Process>& processes,
     mvwprintw(window, row, ram_column, processes[i].Ram().c_str());
     mvwprintw(window, row, time_column,
               Format::ElapsedTime(processes[i].UpTime()).c_str());
+    // mvwprintw(window, row, command_column, processes[i].Command().substr(0,
+    // window->_maxx - 46).c_str());
     mvwprintw(window, row, command_column,
-              processes[i].Command().substr(0, window->_maxx - 46).c_str());
+              processes[i].Command().substr(0, getmaxx(stdscr) - 46).c_str());
   }
 }
 
@@ -90,8 +94,9 @@ void NCursesDisplay::Display(System& system, int n) {
 
   int x_max{getmaxx(stdscr)};
   WINDOW* system_window = newwin(9, x_max - 1, 0, 0);
-  WINDOW* process_window =
-      newwin(3 + n, x_max - 1, system_window->_maxy + 1, 0);
+  // WINDOW* process_window = newwin(3 + n, x_max - 1, system_window->_maxy + 1,
+  // 0);
+  WINDOW* process_window = newwin(3 + n, x_max - 1, 9 + 1, 0);
 
   while (1) {
     init_pair(1, COLOR_BLUE, COLOR_BLACK);
